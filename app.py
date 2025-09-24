eef63731b11012586d8dfac07aecf0cb6f5859a0
thanawanngam-jpg <thanawanngam@gmail.com>
2025-09-24 21:29:52 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ model = YOLO("yolo11n.pt")
 # Upload image
 uploaded_image = st.file_uploader("Upload an image (jpg , png)", type=["jpg", "jpeg", "png"])
 
-if uploaded_image is not None
+if uploaded_image is not None:
 
   # Show original image
   st.image(uploaded_image , caption="Uploaded Image", use_container_width=True)
